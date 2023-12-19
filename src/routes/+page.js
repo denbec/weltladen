6000b3cd4aa7a3241059ea9f1da57afea8d8f17c
dennis <becker@lotum.de>
2023-12-19 15:57:29 +0100

    Add Directus binding

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,1 +1,11 @@
+/** @type {import('./$types').PageLoad} */
+import getDirectusInstance from '$lib/directus';
+import { readItems } from '@directus/sdk';
+
 export const prerender = true;
+export async function load({ fetch }) {
+	const directus = getDirectusInstance(fetch);
+	return {
+		global: await directus.request(readItems('global'))
+	};
+}
